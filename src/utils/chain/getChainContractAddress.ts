56df57d7f91bb1d2bce73e487dd2feffad6be65a
Marco-TGOL <144946998+Marco-TGOL@users.noreply.github.com>
2023-11-17 17:58:18 -0300

    Update getChainContractAddress.ts
    deleted space

--- src/utils/chain/getChainContractAddress.ts
+++ src/utils/chain/getChainContractAddress.ts
@@ -7,5 +7,5 @@ export type GetChainContractAddressErrorType =
 
 export function getChainContractAddress(
 ) {
-  return '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619 '
+  return '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619';
 }
